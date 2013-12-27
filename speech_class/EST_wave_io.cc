6b4d4c0c77146981567dcf48e0d943cfb9794a3f
Sergio Oller <sergioller@gmail.com>
2013-12-27 14:16:42 +0100

    [Fix] Set NIST_SIG as character array in order to use sizeof to obtain its length.

--- speech_class/EST_wave_io.cc
+++ speech_class/EST_wave_io.cc
@@ -62,8 +62,8 @@ static int def_load_sample_rate = 16000;
 /* Sphere Nist files                                                     */
 /*=======================================================================*/
 
-static const char *NIST_SIG = "NIST_1A\n   1024\n";
-static const char *NIST_END_SIG = "end_head\n";
+static const char NIST_SIG[] = "NIST_1A\n   1024\n";
+static const char NIST_END_SIG[] = "end_head\n";
 #define NIST_HDR_SIZE 1024
 
 int nist_get_param_int(const char *hdr, const char *field, int def_val)
@@ -182,7 +182,7 @@ enum EST_read_status load_wave_nist(EST_TokenStream &ts, short **data, int
     if (ts.fread(header,NIST_HDR_SIZE,1) != 1)
 	return wrong_format;
 
-    if (strncmp(header,NIST_SIG,sizeof(NIST_SIG)) != 0)
+    if (strncmp(header,NIST_SIG,sizeof(NIST_SIG)-1) != 0)
 	return wrong_format;
 
     samps = nist_get_param_int(header,"sample_count",-1);
