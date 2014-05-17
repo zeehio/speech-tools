0785c95589bf6197166f289b416e3e8e14e2e7e0
Sergio Oller <sergioller@gmail.com>
2014-05-17 14:34:10 +0200

    Fix: Avoid useless copying (pass by ref)

--- include/EST_Wagon.h
+++ include/EST_Wagon.h
@@ -105,8 +105,9 @@ class WQuestion {
     EST_IList operandl;
     float score;
   public:
-    WQuestion() {;}
-    WQuestion(const WQuestion &s) 
+    WQuestion() {
+        this->score = WGN_HUGE_VAL;}
+    WQuestion(const WQuestion &s)
        { feature_pos=s.feature_pos;
          op=s.op; yes=s.yes; no=s.no; operand1=s.operand1;
 	 operandl = s.operandl; score=s.score;}
